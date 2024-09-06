b16d5a3891bc31c13de6efcb050a1a8142ab93fd
Matheus Lasserre <matheuslasserre3@gmail.com>
2024-09-06 13:44:25 -0300

    CircleInOut

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,6 @@ export default defineConfig({
                 'gsap'
             ]
         }
-    }
+    },
+    prefetch: true
 });
--- src/store/utils.ts
+++ src/store/utils.ts
@@ -1,13 +1,18 @@
-import { gsapStore, localTimeline } from "./store";
+import { gsapStore, localTimeline, pageTimelineStore } from "./store";
 
 
-export function Init (): [typeof globalThis.gsap, gsap.core.Timeline, (updatedTimeline: gsap.core.Timeline) => void] {
+export function Init (): [typeof globalThis.gsap, gsap.core.Timeline, (updatedTimeline: gsap.core.Timeline) => void, gsap.core.Timeline, (updatedTimeline: gsap.core.Timeline) => void] {
     const setTimeLine = (updatedTimeline: gsap.core.Timeline) => {
         localTimeline.set(updatedTimeline)
+    }
+    const setPageTimeLine = (updatedTimeline: gsap.core.Timeline) => {
+        pageTimelineStore.set(updatedTimeline)
     }
 	return [
 		gsapStore.get(),
 		localTimeline.get(),
-        setTimeLine
+        setTimeLine,
+        pageTimelineStore.get(),
+        setPageTimeLine
 	]
   }
